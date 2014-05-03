17745c897922428201367c39cf0f1065fa4edbbf
xiaoqiangkx <xiaoqiangkx@gmail.com>
2014-05-03 11:18:21 +0800

    add ex12_41~42

---
+++ exercise/chapter12/ex12_42.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class X {
+    public:
+    const static int a = 1;
+};
+
+const int X::a;
+
+int main() {
+    X x;
+    cout << x.a << endl;
+    return 0;
+}
